20205d750facb870c1f94076c9b0eef349529fea
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-30 09:40:24 -0400

    added the starter code for CardBody.js

--- cards-to-components-react/src/components/CardBody.js
+++ cards-to-components-react/src/components/CardBody.js
@@ -1,2 +1,18 @@
+// 4 added in the starter code for CardBody.js
 import React from "react"
 
+const CardBody = () => {
+    return (
+      <div className="card-body">
+          <h5 className="card-title">Card 1</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <Button />
+        </div>
+    );
+  };
+  
+  //export the component
+  export default CardBody

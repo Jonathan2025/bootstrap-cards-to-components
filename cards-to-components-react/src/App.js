1594211739ad268349a34959002815faea61a3e1
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-30 09:28:23 -0400

    created a new react app and added in the app.js starter code from lecture

---
+++ cards-to-components-react/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+//import card1 and 2 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bootcamp Cards to Component Example</h1>
+      <section className="cards">
+        <Card1 /> 
+        <Card2 />
+      </section>
+    </div>
+  );
+}
+
+export default App;

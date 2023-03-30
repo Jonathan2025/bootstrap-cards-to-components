2b19dec2166977563c5ad3c00ed749153860a32b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-30 09:38:34 -0400

    added in the starter code for Button.js

---
+++ cards-to-components-react/src/components/Button.js
@@ -0,0 +1,10 @@
+// 3 added in the starter code for Button.js
+import React from 'react'
+
+const Button = () => {
+  return (
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  )
+}
+
+export default Button
